97a16ce228ef1e535f7f878836c0a65f1a2c7315
Alemneh <Alemneh@hotmail.com>
2017-04-17 20:14:30 -0700

    added error handling

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node --harmony
 'use strict';
+const request = require('superagent');
 const co = require('co');
 const prompt = require('co-prompt');
 const program = require('commander');
@@ -13,8 +14,29 @@ program
     co(function *() {
       var username = yield prompt('username: ');
       var password = yield prompt.password('password: ');
-      console.log('user: %s pass: %s file: %s',
-          username, password, file);
+      request
+        .post('https://api.bitbucket.org/2.0/snippets/')
+        .auth(username, password)
+        .attach('file', file)
+        .set('Accept', 'application/json')
+        .end(function (err, res) {
+          if (!err && res.ok) {
+            var link = res.body.links.html.href;
+            console.log('Snippet created: %s', link);
+            process.exit(0);
+          }
+
+          var errorMessage;
+          if (res && res.status == 401) {
+            errorMessage = 'Authenticate failed! Bad username/password?';
+          } else if (err) {
+            errorMessage = err;
+          } else {
+            errorMessage = res.text;
+          }
+          console.error(errorMessage);
+          process.exit(1);
+        });
     });
 
   })
